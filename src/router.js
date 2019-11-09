2cffa96c32bb6df43c3235054e8f818ad0719de5
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2019-11-09 16:35:22 +0400

    add views templates
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -10,6 +9,65 @@ export default new Router({
   routes: [{
     path: '/',
     name: 'home',
-    component: Home,
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('./views/Home.vue'),
+  }, {
+    path: '/login',
+    name: 'login',
+    meta: {
+      layout: 'empty',
+    },
+    component: () => import('./views/Login.vue'),
+  }, {
+    path: '/categories',
+    name: 'categories',
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('./views/Categories.vue'),
+  }, {
+    path: '/register',
+    name: 'register',
+    meta: {
+      layout: 'empty',
+    },
+    component: () => import('./views/Register.vue'),
+  }, {
+    path: '/detail',
+    name: 'detail',
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('./views/Detail.vue'),
+  }, {
+    path: '/history',
+    name: 'history',
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('./views/History.vue'),
+  }, {
+    path: '/planning',
+    name: 'planning',
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('./views/Planning.vue'),
+  }, {
+    path: '/profile',
+    name: 'profile',
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('./views/Profile.vue'),
+  }, {
+    path: '/record',
+    name: 'record',
+    meta: {
+      layout: 'main',
+    },
+    component: () => import('./views/Record.vue'),
   }],
 });
