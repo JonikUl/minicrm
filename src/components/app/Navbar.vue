d93045e4f5c62e6a76d0a3f6eaa17a9d62d01eed
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2019-11-09 20:00:15 +0400

    add date filter
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,25 +5,25 @@
         <a href="#" @click.prevent="$emit('click')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{ date | date('datetime') }}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
         <li>
-          <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
+          <a class="dropdown-trigger black-text" href="#" data-target="dropdown" ref="dropdown">
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
           </a>
 
           <ul id="dropdown" class="dropdown-content">
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -33,3 +33,32 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    date: new Date(),
+    interval: null,
+    dropdown: null
+  }),
+  methods: {
+    logout() {
+      this.$router.push("/login?message=logout");
+    }
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.date = new Date();
+    }, 1000);
+    this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: false
+    });
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+    if (this.dropdown && this.destroy) {
+      this.dropdown.destroy();
+    }
+  }
+};
+</script>
---
+++ src/filters/date.filter.js
@@ -0,0 +1,15 @@
+export default function dateFilter(value, format = 'date') {
+  const options = {};
+  if (format.includes('date')) {
+    options.day = '2-digit';
+    options.month = 'long';
+    options.year = 'numeric';
+  }
+  if (format.includes('time')) {
+    options.hour = '2-digit';
+    options.minute = '2-digit';
+    options.second = '2-digit';
+  }
+
+  return new Intl.DateTimeFormat('ru-Ru', options).format(new Date(value));
+}
