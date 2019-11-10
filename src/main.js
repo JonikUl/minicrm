ad890e854974e12ca03576855e74a5873fd53587
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2019-11-10 09:55:41 +0400

    add firebase
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- src/main.js
+++ src/main.js
@@ -7,12 +7,26 @@ import dateFilter from '@/filters/date.filter';
 import messagePlugin from '@/utils/message.plugin';
 import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 Vue.config.productionTip = false;
 
 Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLpK07KSq38MAlSAXNbR-UP4V96DFg9CI",
+  authDomain: "vue-crm-money.firebaseapp.com",
+  databaseURL: "https://vue-crm-money.firebaseio.com",
+  projectId: "vue-crm-money",
+  storageBucket: "vue-crm-money.appspot.com",
+  messagingSenderId: "442527604134",
+  appId: "1:442527604134:web:ffb004960b232203cd8367",
+  measurementId: "G-BJCJCDSLWN"
+};
 
 new Vue({
   router,
