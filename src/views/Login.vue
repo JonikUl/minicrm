16ed29f5c5c0f934bb948b189d9b279477e20dfb
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2019-11-10 09:38:16 +0400

    add notifications
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -33,6 +33,8 @@
 
 <script>
 import { email, required, minLength } from "vuelidate/lib/validators";
+import messages from '@/utils/messages';
+
 export default {
   name: "login",
   data: () => ({
@@ -42,6 +44,11 @@ export default {
   validations: {
     email: { email, required },
     password: { required, minLength: minLength(6) }
+  },
+  mounted(){
+    if (messages[this.$route.query.message]) {
+      this.$message(messages[this.$route.query.message]);
+    }
   },
   methods: {
     submitHandler() {
