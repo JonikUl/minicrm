179f3fd64ee6b2d7d7a9f374c1bc0bcf8322ddf5
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2019-11-16 12:23:03 +0400

    add record details
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- src/store/category.js
+++ src/store/category.js
@@ -20,6 +20,25 @@ export default {
         throw e;
       }
     },
+    async fetchCategoryById({
+      commit,
+      dispatch,
+    }, id) {
+      try {
+        const uid = await dispatch('getUid');
+        const category = (await firebase.database().ref(`/users/${uid}/categories`).child(id)
+          .once(
+            'value',
+          )).val() || {};
+        return {
+          ...category,
+          id,
+        };
+      } catch (e) {
+        commit('setError', e);
+        throw e;
+      }
+    },
     async updateCategory({
       commit,
       dispatch,
