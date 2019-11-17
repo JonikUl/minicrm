ba2cf5cae342be3ab2e5665f3bf967ae31e80878
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2019-11-17 14:02:23 +0400

    add record localization
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -3,8 +3,11 @@
     <Loader v-if="loading" />
     <div v-else-if="record">
       <div class="breadcrumb-wrap">
-        <router-link to="/history" class="breadcrumb">История</router-link>
-        <a @click.prevent class="breadcrumb">{{ record.type === 'income' ? 'Доход' : 'Расход' }}</a>
+        <router-link to="/history" class="breadcrumb">{{ 'Menu_History' | localize}}</router-link>
+        <a
+          @click.prevent
+          class="breadcrumb"
+        >{{ record.type === 'income' ? localizeFilter('Income') : localizeFilter('Outcome') }}</a>
       </div>
       <div class="row">
         <div class="col s12 m6">
@@ -16,20 +19,22 @@
           }"
           >
             <div class="card-content white-text">
-              <p>Описание: {{ record.description }}</p>
-              <p>Сумма: {{ record.amount | currency}}</p>
-              <p>Категория: {{ record.categoryName }}</p>
+              <p>{{ 'Description' | localize }}: {{ record.description }}</p>
+              <p>{{ 'Amount' | localize }}: {{ record.amount | currency}}</p>
+              <p>{{ 'Category' | localize }}: {{ record.categoryName }}</p>
               <small>{{ record.date | date('datetime') }}</small>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <p class="center" v-else>Такой записи не существует</p>
+    <p class="center" v-else>{{ 'NoSuchEntry' | localize }}</p>
   </div>
 </template>
 
 <script>
+import localizeFilter from "../filters/localize.filter";
+
 export default {
   name: "detail",
   data: () => ({
@@ -48,6 +53,9 @@ export default {
       categoryName: category.title
     };
     this.loading = false;
+  },
+  methods: {
+    localizeFilter
   }
 };
 </script>
